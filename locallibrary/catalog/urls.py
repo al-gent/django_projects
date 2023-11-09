d1fe323b73753992b42a07d0c14eeb68f15e3167
Adam Gent <94gent@gmail.com>
2023-11-09 09:20:49 -0800

    day 3

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 
-#the name parameter is included here so that we can refer to this URL mapping in other parts of the code
-#for when we want to create links to this page in our templates
-#it would look like: {% url 'index' %}
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
@@ -13,5 +10,5 @@ urlpatterns = [
     path('authors/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     #<int:pk> is a path converter that matches an integer and passes it to the view as a variable called pk
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-    
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
